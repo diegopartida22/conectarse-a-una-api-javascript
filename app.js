07cf7fe6b654021b6a167e6a2cacb1bd90d0f214
Diego <diegopartidaromero@gmail.com>
2023-03-06 05:57:41 -0600

    API

--- app.js
+++ app.js
@@ -1,55 +1,40 @@
-let pagina = 1;
-const btnAnterior = document.getElementById('btnAnterior');
-const btnSiguiente = document.getElementById('btnSiguiente');
+// fetch nos permite hacer una petición a la siguiente dirección, movie/550 es el endpoint
+// when we use fetch it returns a promise, a promise means thet we are creating a petition, el servidor esta haciendo recibiendo la peticion para procesarla y esperar a que termine
+// usamos el await para decirle que haga la petición y cuando termine ahora si pase a la siguiente acción, solo se puede usar await en funciones asyncronas
+// el try/catch que siempre que se utiliza async await, se usa el try/catch y lo que hace es intentar resolver un programa y en caso de tener un error el catch lo ejecuta
 
-btnSiguiente.addEventListener('click', () => {
-	if(pagina < 1000){
-		pagina += 1;
-		cargarPeliculas();
-	}
-});
+const loadMovies = async () => {
+  try {
+    const answer = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=0e73770fb198963fab9ff92aa5c0b1f2&language=es-MX"
+    );
+    console.log(answer);
 
-btnAnterior.addEventListener('click', () => {
-	if(pagina > 1){
-		pagina -= 1;
-		cargarPeliculas();
-	}
-});
+    // con el methodo json(); podemos acceder a la informacion que nos devolvio la peticion, tambien es asyncrono
+    if (answer.status === 200) {
+      const data = await answer.json();
+      // por cada pelicula me vas a ejecutar una funcion
+      let movies = "";
+      data.results.forEach((movie) => {
+        movies += `
+			<div class="pelicula">
+				<img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+				<h3 class="titulo">${movie.title}</h3">
+			</div>
+		`;
+      });
 
-const cargarPeliculas = async() => {
-	try {
-		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
-	
-		console.log(respuesta);
+      document.getElementById("contenedor").innerHTML = movies;
+    } else if (answer.status === 401) {
+      console.log("Wrong key");
+    } else if (answer.status === 404) {
+      console.log("The movie doesn't exist");
+    } else {
+      console.log("Unknown error");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-		// Si la respuesta es correcta
-		if(respuesta.status === 200){
-			const datos = await respuesta.json();
-			
-			let peliculas = '';
-			datos.results.forEach(pelicula => {
-				peliculas += `
-					<div class="pelicula">
-						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-						<h3 class="titulo">${pelicula.title}</h3>
-					</div>
-				`;
-			});
-
-			document.getElementById('contenedor').innerHTML = peliculas;
-
-		} else if(respuesta.status === 401){
-			console.log('Pusiste la llave mal');
-		} else if(respuesta.status === 404){
-			console.log('La pelicula que buscas no existe');
-		} else {
-			console.log('Hubo un error y no sabemos que paso');
-		}
-
-	} catch(error){
-		console.log(error);
-	}
-
-}
-
-cargarPeliculas();
+loadMovies();
